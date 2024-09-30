a308cdf4d6b83ff58eed82840acbd177cdf4ac32
[CLOUD]나용진 <yjnaga@naver.com>
2024-09-30 12:42:13 +0900

    Initial commit

---
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
@@ -0,0 +1,13 @@
+package com.team04.buy_gurus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuyGurusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuyGurusApplication.class, args);
+	}
+
+}
---
+++ buy-gurus-b/src/test/java/com/team04/buy_gurus/BuyGurusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team04.buy_gurus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyGurusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
