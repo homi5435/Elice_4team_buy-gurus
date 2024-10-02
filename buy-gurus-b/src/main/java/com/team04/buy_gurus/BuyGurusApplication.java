670524153c2675ce3d877dcc8aa38e54d566a227
PromotezCitizen <gkstkdgus821@gmail.com>
2024-10-02 16:45:30 +0900

    Config: 기본 설정
    Spring Security에서 모든 경로 허용
    Auditing을 위한 어노테이션 추가
    h2 사용을 위한 yaml 파일 수정

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
@@ -2,8 +2,10 @@ package com.team04.buy_gurus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BuyGurusApplication {
 
 	public static void main(String[] args) {
