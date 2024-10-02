85deea3def34cc881e96ab2c6923d958d7de39fd
PromotezCitizen <gkstkdgus821@gmail.com>
2024-10-02 16:49:28 +0900

    Feat: 유저의 배송지 정보를 담는 End Point 작성

---
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/address/dto/AddressInfoRequest.java
@@ -0,0 +1,10 @@
+package com.team04.buy_gurus.address.dto;
+
+import lombok.Getter;
+
+@Getter
+public class AddressInfoRequest {
+    private String name;
+    private String address;
+    private String phoneNum;
+}
