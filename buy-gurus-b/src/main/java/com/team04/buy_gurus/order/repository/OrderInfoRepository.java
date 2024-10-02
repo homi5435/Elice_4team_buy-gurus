3cf31b4c9dbd5328099e655095adf2363b664aff
[CLOUD4] 한상현 <gkstkdgus821@gmail.com>
2024-10-02 10:47:38 +0000

    Revert "Merge branch '2-feat-end-point' into 'master'"
    This reverts merge request !2

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.team04.buy_gurus.order.repository;
-
-import com.team04.buy_gurus.order.domain.OrderInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
-}
