188c873148416c334777e265912c0b41f117c786
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-10-03 17:20:42 +0900

    feat : categoryService 생성

---
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/category/application/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.team04.buy_gurus.category.application.service;
+
+import com.team04.buy_gurus.category.application.dto.CategoryDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@Service
+public class CategoryService {
+    public List<CategoryDto> findAll(){
+
+    }
+}
