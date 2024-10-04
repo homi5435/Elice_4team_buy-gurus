d49c9dfd737c412351bf6ba665069429b077c0af
[CLOUD4] 유호성 <hhss5435@gmail.com>
2024-10-04 18:12:12 +0900

    fix: "responseDto builder추가"
    1. responseDto 빌더 추가하여 service에서 사용

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/product/dto/ProductResponse.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/product/dto/ProductResponse.java
@@ -1,11 +1,16 @@
 package com.team04.buy_gurus.product.dto;
 
 import com.team04.buy_gurus.product.domain.Product;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class ProductResponse {
-    private Long productId;
+    private Long id;
     private String name;
     private Long price;
     private String description;
@@ -14,7 +19,7 @@ public class ProductResponse {
     private String category;
 
     public ProductResponse(Product product){
-        this.productId = product.getId();
+        this.id = product.getId();
         this.name = product.getName();
         this.price = product.getPrice();
         this.description = product.getDescription();
