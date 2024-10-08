b702dbbce6e9bc84df40f9bc7cd0cf4ed3cddee0
[CLOUD4] 홍민영 <47109@naver.com>
2024-10-08 18:11:06 +0900

    Fix: React CORS 수정

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
@@ -2,6 +2,7 @@ package com.team04.buy_gurus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -14,7 +15,7 @@ public class BuyGurusApplication {
 	}
 
 	// Cross-Origin Resource Sharing (CORS)
-	// http://127.0.0.1:5173/ 으로 부터 오는 모든 요청 허용하기
+	// http://localhost:5173/ 으로 부터 오는 모든 요청 허용하기
 	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
@@ -22,8 +23,9 @@ public class BuyGurusApplication {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**") // 모든 경로에 대한 HTTP 요청을 처리
 						.allowedMethods("*") // 모든 http 메소드를 허용
-						.allowedOrigins("http://127.0.0.1:5173"); // "http://127.0.0.1:5173"에서 오는 요청만 허용
+						.allowedOrigins("http://localhost:5173"); // "http://localhost:5173"에서 오는 요청만 허용
 			}
 		};
 	}
+
 }
