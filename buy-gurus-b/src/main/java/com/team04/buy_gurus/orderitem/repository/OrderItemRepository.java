bca4b08dda05c0e791b76846050ed8799c940470
[CLOUD4] 홍민영 <47109@naver.com>
2024-10-08 18:10:27 +0900

    Fix: Repository deleteAllByUser 수정

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/orderitem/repository/OrderItemRepository.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/orderitem/repository/OrderItemRepository.java
@@ -1,10 +1,10 @@
 package com.team04.buy_gurus.orderitem.repository;
 
 import com.team04.buy_gurus.orderitem.domain.OrderItem;
-import com.team04.buy_gurus.orderitem.dto.OrderItemViewDto;
 import com.team04.buy_gurus.product.domain.Product;
 import com.team04.buy_gurus.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
@@ -14,8 +14,9 @@ import java.util.List;
 public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
     OrderItem findByUserAndProduct(User user, Product product);
 
-    List<OrderItemViewDto> findByUser(User user);
+    List<OrderItem> findByUser(User user);
 
-    @Query("delete from OrderItem o where o.user = :user")
-    void deleteAllByUser(User user);
+    @Modifying
+    @Query("delete from OrderItem o where o.user.id = :user_id")
+    void deleteAllByUser(Long user_id);
 }
