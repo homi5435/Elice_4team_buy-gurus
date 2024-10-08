67358bb9261bbd68f356ca27e186b7354caa8729
[CLOUD4] 홍민영 <47109@naver.com>
2024-10-08 18:35:44 +0900

    Fix: React CORS 수정

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/BuyGurusApplication.java
@@ -2,12 +2,13 @@ package com.team04.buy_gurus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BuyGurusApplication {
 
 	public static void main(String[] args) {
