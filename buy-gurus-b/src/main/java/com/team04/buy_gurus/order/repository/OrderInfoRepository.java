d168807866e93e1876b02ec5989c71cb283f9891
[CLOUD4] 유호성 <hhss5435@gmail.com>
2024-10-10 18:41:26 +0900

    fix: "리뷰 추가시 유효성 검증 수정"
    - OrderRepository: 유저, 상태 확인
    - OrderInfoRepository: 상품 확인

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderInfoRepository.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderInfoRepository.java
@@ -1,7 +1,11 @@
 package com.team04.buy_gurus.order.repository;
 
+import com.team04.buy_gurus.order.domain.Order;
 import com.team04.buy_gurus.order.domain.OrderInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
+    Optional<OrderInfo> findByProductId(Long productId);
 }
