e1e7499331be3b1b7dd438fd5c9608d443134c7d
PromotezCitizen <gkstkdgus821@gmail.com>
2024-10-15 13:11:00 +0900

    Update: Repository 메서드 위치 이동
    Review에서 주문 했는지 확인용으로 사용하는 경우 OrderInfoRepository에서 획득

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderInfoRepository.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderInfoRepository.java
@@ -1,11 +1,15 @@
 package com.team04.buy_gurus.order.repository;
 
-import com.team04.buy_gurus.order.domain.Order;
 import com.team04.buy_gurus.order.domain.OrderInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderInfoRepository extends JpaRepository<OrderInfo, Long> {
     Optional<OrderInfo> findByProductId(Long productId);
+
+    @Query("SELECT oi FROM OrderInfo oi JOIN oi.order o WHERE oi.product.id = :productId AND oi.isReviewed = false AND oi.isDeleted = false")
+    List<OrderInfo> findOrderInfoByProductId(Long productId);
 }
--- buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderRepository.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/order/repository/OrderRepository.java
@@ -4,13 +4,7 @@ import com.team04.buy_gurus.order.domain.Order;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.Repository;
-
-import java.util.Optional;
 
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    //리뷰 주문 정보 조회
-//    Optional<Order> findByUserIdAndProductIdAndStatus(Long userId, Long productId, String status);
-
     Page<Order> findAllByIsDeletedFalse(Pageable pageable);
 }
