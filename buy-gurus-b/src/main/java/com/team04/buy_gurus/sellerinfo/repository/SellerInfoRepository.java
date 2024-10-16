a272428c98e75acd066b4049a0e3e135ec0d8043
PromotezCitizen <gkstkdgus821@gmail.com>
2024-10-16 14:18:07 +0900

    Feat: 이메일로 Seller를 찾는 로직 추가

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/sellerinfo/repository/SellerInfoRepository.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/sellerinfo/repository/SellerInfoRepository.java
@@ -1,9 +1,15 @@
 package com.team04.buy_gurus.sellerinfo.repository;
 
 import com.team04.buy_gurus.sellerinfo.entity.SellerInfo;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SellerInfoRepository extends JpaRepository<SellerInfo, Long> {
+    @Query("SELECT si FROM SellerInfo si JOIN si.user u WHERE u.email=:email")
+    Optional<SellerInfo> findByUseremail(@Param("email") String email);
 }
