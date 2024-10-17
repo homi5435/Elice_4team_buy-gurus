33dbabd7bfa7de15bb960ae299c4c5691cd432ab
[CLOUD4] 유호성 <hhss5435@gmail.com>
2024-10-17 18:44:10 +0900

    feat: "상품 임시 정보 추가"
    - 상품 임시 정보 추가

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/product/dto/ProductResponse.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/product/dto/ProductResponse.java
@@ -3,32 +3,37 @@ package com.team04.buy_gurus.product.dto;
 import com.team04.buy_gurus.product.domain.Product;
 
 import com.team04.buy_gurus.product.domain.ProductImage;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
     private Long id;
     private String name;
     private Long price;
     private String description;
     private Long quantity;
-    private List<String> imageUrls;
+    private List<String> imageUrls = new ArrayList<>();
     private String category;
 
-    public ProductResponse(Product product, ProductImage productImage){
+    public ProductResponse(Product product, List<ProductImage> productImages) {
         this.id = product.getId();
         this.name = product.getName();
         this.price = product.getPrice();
         this.description = product.getDescription();
         this.quantity = product.getQuantity();
-        this.imageUrls = Collections.singletonList(productImage.getImageUrl());
         this.category = product.getCategory();
+//        this.category = product.getCategory() != null ? product.getCategory().getName() : null; // 카테고리 이름 처리
+        this.imageUrls = new ArrayList<>(); // 빈 리스트 초기화
+
+        for (ProductImage productImage : productImages) {
+            this.imageUrls.add(productImage.getImageUrl());
+        }
     }
 }
