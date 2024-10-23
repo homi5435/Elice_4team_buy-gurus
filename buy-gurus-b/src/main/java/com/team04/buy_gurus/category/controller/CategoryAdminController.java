2189ace03f9c88ecfa16fdbd83748aa313e0b8e5
[CLOUD4] 김다애 <wlsemrdl1212@naver.com>
2024-10-23 21:02:13 +0900

    카테고리 생성시 데이터 전송 수정

--- buy-gurus-b/src/main/java/com/team04/buy_gurus/category/controller/CategoryAdminController.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/category/controller/CategoryAdminController.java
@@ -1,6 +1,8 @@
 package com.team04.buy_gurus.category.controller;
 
 import com.team04.buy_gurus.category.dto.CategoryCreateRequest;
+import com.team04.buy_gurus.category.dto.response.CategoryFirstCreatedResponse;
+import com.team04.buy_gurus.category.dto.response.CategorySubCreatedResponse;
 import com.team04.buy_gurus.category.dto.response.Response;
 import com.team04.buy_gurus.category.service.CategoryService;
 import jakarta.validation.Valid;
@@ -25,15 +27,14 @@ public class CategoryAdminController {
     }
     //최초의 대분류 카테고리를 생성하는 로직
     @PostMapping("/FirstCategory")
-    public Response createFirstCategory(@Valid @RequestBody final CategoryCreateRequest request){
-        categoryService.createCategory(request.getName());
-        return Response.success();
+    public Response createFirstCategory(@Valid @RequestBody final CategoryCreateRequest request) {
+        CategoryFirstCreatedResponse response = categoryService.createCategory(request.getName());
+        return Response.success(response);
     }
-
 
     @PostMapping("/category")
     public Response createCategory(@Valid @RequestBody final CategoryCreateRequest request){
-        categoryService.updateCategory(request);
-        return Response.success();
+        CategorySubCreatedResponse response = categoryService.updateCategory(request);
+        return Response.success(response);
     }
 }
--- buy-gurus-b/src/main/java/com/team04/buy_gurus/category/dto/response/Response.java
+++ buy-gurus-b/src/main/java/com/team04/buy_gurus/category/dto/response/Response.java
@@ -9,16 +9,16 @@ import lombok.Getter;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Getter
-public class Response {
+public class Response<T> {
 
     private boolean success;
     private int code;
-    private Result result;
+    private T result;
 
 
     public static Response success(){return new Response(true, 0, null);}
 
-    public static <T> Response success(T data){return new Response(true, 0, null);}
+    public static <T> Response success(T data){return new Response(true, 0, data);}
 
     public static Response failure(int code, String msg){ return new Response(false, code, new Failure(msg));}
 }
